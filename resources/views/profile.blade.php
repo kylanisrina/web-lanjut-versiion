b2c7a2895e70646303addd4c680898591b310342
kylanisrina <kylanisrina02@gmail.com>
2024-10-12 00:45:35 +0700

    Modul 6

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.app3')
 @section ('content')
-    <div class="card">
-        <img src="/asset/images/ming.jpg" alt="Profile Picture" class="profile-img">
-        <div class="info">
-            <h1 class="label">{{$nama}}</h1>
-            <h1 class="label">{{ $npm }}</h1>
-            <h1 class="label">{{ $nama_kelas }}</h1>
-        </div>
+<div class="card">
+    <img src="{{ Storage::url($user->foto) }}" alt="Profile Picture" class="profile-img"></img>
+    <div class="info">
+        <h1 class="label">{{ $user->nama }} </h1>
+        <h1 class="label">{{ $user->npm }}  </h1>
+        <h1 class="label">{{ $user->nama_kelas  ?? 'Kelas tidak ditemukan' }}</h1>
     </div>
+</div>
+{{-- // jangan lupa kasih perintah php artisan storage:link --}}
 
 @endsection
